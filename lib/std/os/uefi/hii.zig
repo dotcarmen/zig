866c4017bb778b8d07cd0eeac3fe9d8eaa774f3c
Carmen <carmen@dotcarmen.dev>
2025-03-19 15:36:26 +0900

    std.os.uefi: complete hii typing

--- lib/std/os/uefi/hii.zig
+++ lib/std/os/uefi/hii.zig
@@ -1,4 +1,5 @@
-const uefi = @import("std").os.uefi;
+const std = @import("std");
+const uefi = std.os.uefi;
 const Guid = uefi.Guid;
 
 pub const Handle = *opaque {};
@@ -6,21 +7,84 @@ pub const Handle = *opaque {};
 /// The header found at the start of each package.
 pub const PackageHeader = packed struct(u32) {
     length: u24,
-    type: u8,
+    type: PackageType,
 
-    pub const type_all: u8 = 0x0;
-    pub const type_guid: u8 = 0x1;
-    pub const forms: u8 = 0x2;
-    pub const strings: u8 = 0x4;
-    pub const fonts: u8 = 0x5;
-    pub const images: u8 = 0x6;
-    pub const simple_fonsts: u8 = 0x7;
-    pub const device_path: u8 = 0x8;
-    pub const keyboard_layout: u8 = 0x9;
-    pub const animations: u8 = 0xa;
-    pub const end: u8 = 0xdf;
-    pub const type_system_begin: u8 = 0xe0;
-    pub const type_system_end: u8 = 0xff;
+    pub fn getDataBytes(self: *PackageHeader) ?[]u8 {
+        if (self.length == @sizeOf(PackageHeader)) return null;
+        const ptr: [*]u8 = @ptrCast(&self._data);
+        const size: usize = @sizeOf(PackageHeader);
+        return ptr[size .. size + self.length];
+    }
+
+    pub fn getData(self: *PackageHeader) PackageType.Data {
+        switch (self.type) {
+            inline .type_all, .end, .type_system_begin, .type_system_end => |tag| {
+                return @unionInit(PackageType.Data, tag, {});
+            },
+            .type_guid => {
+                const bytes = self.getDataBytes().?;
+                const guid: *const Guid = @ptrCast(bytes[0..16].ptr);
+                const data: *const anyopaque = @ptrCast(bytes[16..].ptr);
+                return @unionInit(PackageType.Data, .type_guid, .{
+                    .guid = guid,
+                    .data = data,
+                });
+            },
+            inline else => |tag| {
+                const Payload = std.meta.TagPayload(PackageType.Data, tag);
+                const payload: Payload = @ptrCast(self.getDataBytes().?.ptr);
+                return @unionInit(
+                    PackageType.Data,
+                    @tagName(tag),
+                    payload,
+                );
+            },
+        }
+    }
+
+    pub fn next(self: *PackageHeader) ?*PackageHeader {
+        // The package lists form ... terminated with ... a Type of EFI_HII_PACKAGE_END
+        if (self.type == .end) return null;
+        const ptr: [*]u8 = @ptrCast(self);
+        // When added to a pointer pointing to the start of the header, Length
+        // points at the next package.
+        return @ptrCast(ptr[self.length]);
+    }
+};
+
+pub const PackageType = enum(u8) {
+    type_all = 0x00,
+    type_guid = 0x01,
+    forms = 0x02,
+    strings = 0x04,
+    fonts = 0x05,
+    images = 0x06,
+    simple_fonts = 0x07,
+    device_path = 0x08,
+    keyboard_layout = 0x09,
+    animations = 0x0a,
+    end = 0xdf,
+    type_system_begin = 0xe0,
+    type_system_end = 0xff,
+
+    pub const Data = union(PackageType) {
+        type_all,
+        type_guid: struct {
+            guid: *const Guid,
+            data: *const anyopaque,
+        },
+        forms: *FormPackage,
+        strings: *StringPackage,
+        fonts: *FontPackage,
+        images: *ImagePackage,
+        simple_fonts: *SimpleFontPackage,
+        device_path: *DevicePathPackage,
+        keyboard_layout: *KeyboardLayoutPackage,
+        animations: *AnimationPackage,
+        end,
+        type_system_begin,
+        type_system_end,
+    };
 };
 
 /// The header found at the start of each package list.
@@ -33,41 +97,7 @@ pub const PackageList = extern struct {
     // TODO implement iterator
 };
 
-pub const SimplifiedFontPackage = extern struct {
-    header: PackageHeader,
-    number_of_narrow_glyphs: u16,
-    number_of_wide_glyphs: u16,
-
-    pub fn getNarrowGlyphs(self: *SimplifiedFontPackage) []NarrowGlyph {
-        return @as([*]NarrowGlyph, @ptrCast(@alignCast(@as([*]u8, @ptrCast(self)) + @sizeOf(SimplifiedFontPackage))))[0..self.number_of_narrow_glyphs];
-    }
-};
-
-pub const NarrowGlyphAttributes = packed struct(u8) {
-    non_spacing: bool,
-    wide: bool,
-    _pad: u6 = 0,
-};
-
-pub const NarrowGlyph = extern struct {
-    unicode_weight: u16,
-    attributes: NarrowGlyphAttributes,
-    glyph_col_1: [19]u8,
-};
-
-pub const WideGlyphAttributes = packed struct(u8) {
-    non_spacing: bool,
-    wide: bool,
-    _pad: u6 = 0,
-};
-
-pub const WideGlyph = extern struct {
-    unicode_weight: u16,
-    attributes: WideGlyphAttributes,
-    glyph_col_1: [19]u8,
-    glyph_col_2: [19]u8,
-    _pad: [3]u8 = [_]u8{0} ** 3,
-};
+pub const FormPackage = extern struct {};
 
 pub const StringPackage = extern struct {
     header: PackageHeader,
@@ -76,4 +106,129 @@ pub const StringPackage = extern struct {
     language_window: [16]u16,
     language_name: u16,
     language: [3]u8,
+};
+
+pub const FontPackage = extern struct {
+    header: PackageHeader,
+    hdr_size: u32,
+    glyph_block_offset: u32,
+    cell: GlyphInfo,
+    font_style: FontStyle,
+    _font_family: u16,
+
+    pub fn fontFamily(self: *FontPackage) [*:0]u16 {
+        return @ptrCast(&self._font_family);
+    }
+
+    pub fn block(self: *FontPackage) *GlyphBlock {
+        const ptr: [*]u8 = @ptrCast(self);
+        return @ptrCast(ptr[self.glyph_block_offset..]);
+    }
+
+    pub const FontStyle = packed struct(u32) {
+        // 0x00000001
+        bold: bool = false,
+        // 0x00000002
+        italic: bool = false,
+        _pad0: u14 = 0,
+        // 0x00010000
+        emboss: bool = false,
+        // 0x00020000
+        outline: bool = false,
+        // 0x00040000
+        shadow: bool = false,
+        // 0x00080000
+        underline: bool = false,
+        // 0x00100000
+        dbl_under: bool = false,
+        _pad1: u21 = 0,
+    };
+
+    pub const GlyphBlock = extern struct {
+        block_type: BlockType,
+        _block_body: u8,
+
+        pub fn next(self: *GlyphBlock) ?*GlyphBlock {
+            const bytes: [*]u8 = @ptrCast(self);
+            const next_block: *GlyphBlock = @ptrCast(bytes + @sizeOf(GlyphBlock));
+            if (next_block.block_type == BlockType.end) return null;
+            return next_block;
+        }
+
+        pub const BlockType = enum(u8) {
+            end = 0x00,
+            glyph = 0x10,
+            glyphs = 0x11,
+            glyph_default = 0x12,
+            glyphs_default = 0x13,
+            glyph_variability = 0x14,
+            duplicate = 0x20,
+            skip2 = 0x21,
+            skip1 = 0x22,
+            defaults = 0x23,
+            ext1 = 0x30,
+            ext2 = 0x31,
+            ext4 = 0x32,
+        };
+    };
+
+    pub const GlyphInfo = extern struct {
+        width: u16,
+        height: u16,
+        offset_x: i16,
+        offset_y: i16,
+        advance_x: i16,
+    };
+};
+
+pub const ImagePackage = extern struct {};
+
+pub const SimpleFontPackage = extern struct {
+    header: PackageHeader,
+    number_of_narrow_glyphs: u16,
+    number_of_wide_glyphs: u16,
+
+    pub fn getNarrowGlyphs(self: *SimpleFontPackage) []NarrowGlyph {
+        const bytes: [*]u8 = @ptrCast(self);
+        const glyphs: [*]NarrowGlyph = @ptrCast(bytes + @sizeOf(SimpleFontPackage));
+        return glyphs[0..self.number_of_narrow_glyphs];
+    }
+
+    pub fn getWideGlyphs(self: *SimpleFontPackage) []WideGlyph {
+        const narrow_glyphs = self.getNarrowGlyphs();
+        const glyphs: [*]WideGlyph = @ptrCast(narrow_glyphs.ptr[narrow_glyphs.len + 1]);
+        return glyphs[0..self.number_of_wide_glyphs];
+    }
+};
+
+pub const DevicePathPackage = extern struct {};
+
+pub const KeyboardLayoutPackage = extern struct {};
+
+pub const AnimationPackage = extern struct {};
+
+pub const NarrowGlyph = extern struct {
+    unicode_weight: u16,
+    attributes: Attributes,
+    glyph_col_1: [19]u8,
+
+    pub const Attributes = packed struct(u8) {
+        non_spacing: bool,
+        wide: bool,
+        _pad: u6 = 0,
+    };
+};
+
+pub const WideGlyph = extern struct {
+    unicode_weight: u16,
+    attributes: Attributes,
+    glyph_col_1: [19]u8,
+    glyph_col_2: [19]u8,
+    _pad: [3]u8 = [_]u8{0} ** 3,
+
+    pub const Attributes = packed struct(u8) {
+        non_spacing: bool,
+        wide: bool,
+        _pad: u6 = 0,
+    };
 };
--- lib/std/os/uefi/protocol/hii_database.zig
+++ lib/std/os/uefi/protocol/hii_database.zig
@@ -5,21 +5,28 @@ const Status = uefi.Status;
 const hii = uefi.hii;
 const cc = uefi.cc;
 const Error = Status.Error;
+const Handle = uefi.Handle;
 
 /// Database manager for HII-related data structures.
 pub const HiiDatabase = extern struct {
-    _new_package_list: Status, // TODO
+    pub const NotifyFn = *const fn (u8, *const Guid, *const hii.PackageHeader, hii.Handle, usize) Status;
+
+    _new_package_list: *const fn (*HiiDatabase, *const hii.PackageHeader) callconv(cc) Status,
     _remove_package_list: *const fn (*HiiDatabase, hii.Handle) callconv(cc) Status,
     _update_package_list: *const fn (*HiiDatabase, hii.Handle, *const hii.PackageList) callconv(cc) Status,
     _list_package_lists: *const fn (*const HiiDatabase, u8, ?*const Guid, *usize, [*]hii.Handle) callconv(cc) Status,
     _export_package_lists: *const fn (*const HiiDatabase, ?hii.Handle, *usize, [*]hii.PackageList) callconv(cc) Status,
-    _register_package_notify: Status, // TODO
-    _unregister_package_notify: Status, // TODO
+    _register_package_notify: *const fn (*HiiDatabase, u8, ?*const Guid, NotifyFn, usize, *Handle) callconv(cc) Status,
+    _unregister_package_notify: *const fn (*HiiDatabase, Handle) callconv(cc) Status,
     _find_keyboard_layouts: Status, // TODO
     _get_keyboard_layout: Status, // TODO
     _set_keyboard_layout: Status, // TODO
     _get_package_list_handle: Status, // TODO
 
+    pub const NewPackageListError = uefi.UnexpectedError || error{
+        OutOfResources,
+        InvalidParameter,
+    };
     pub const RemovePackageListError = uefi.UnexpectedError || error{NotFound};
     pub const UpdatePackageListError = uefi.UnexpectedError || error{
         OutOfResources,
@@ -36,6 +43,13 @@ pub const HiiDatabase = extern struct {
         InvalidParameter,
         NotFound,
     };
+    pub const RegisterPackageNotifyError = uefi.UnexpectedError || error{
+        OutOfResources,
+        InvalidParameter,
+    };
+    pub const UnregisterPackageNotifyError = uefi.UnexpectedError || error{NotFound};
+
+    // pub fn newPackageList(self: *HiiDatabase, header: *)
 
     /// Removes a package list from the HII database.
     pub fn removePackageList(self: *HiiDatabase, handle: hii.Handle) !void {
@@ -100,6 +114,40 @@ pub const HiiDatabase = extern struct {
         }
     }
 
+    pub fn registerPackageNotify(
+        self: *HiiDatabase,
+        ty: hii.PackageType,
+        package_guid: *const Guid,
+        notify: NotifyFn,
+        notify_type: NotifyType,
+    ) RegisterPackageNotifyError!Handle {
+        var result: Handle = undefined;
+        switch (self._register_package_notify(
+            self,
+            @bitCast(ty),
+            package_guid,
+            notify,
+            @bitCast(notify_type),
+            &result,
+        )) {
+            .success => return result,
+            .out_of_resources => return Error.OutOfResources,
+            .invalid_parameter => return Error.InvalidParameter,
+            else => |status| return uefi.unexpectedStatus(status),
+        }
+    }
+
+    pub fn unregisterPackageNotify(
+        self: *HiiDatabase,
+        handle: Handle,
+    ) UnregisterPackageNotifyError!void {
+        switch (self._unregister_package_notify(self, handle)) {
+            .success => return,
+            .not_found => return Error.NotFound,
+            else => |status| return uefi.unexpectedStatus(status),
+        }
+    }
+
     pub const guid align(8) = Guid{
         .time_low = 0xef9fc172,
         .time_mid = 0xa1b2,
@@ -107,5 +155,13 @@ pub const HiiDatabase = extern struct {
         .clock_seq_high_and_reserved = 0xb3,
         .clock_seq_low = 0x27,
         .node = [_]u8{ 0x6d, 0x32, 0xfc, 0x41, 0x60, 0x42 },
+    };
+
+    pub const NotifyType = packed struct(usize) {
+        new_pack: bool,
+        remove_pack: bool,
+        export_pack: bool,
+        add_pack: bool,
+        _pad: std.meta.Int(.unsigned, @typeInfo(usize).int.bits - 4),
     };
 };
